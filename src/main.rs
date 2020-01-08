8f061b0e6acd14df22a52120ddc4e8c4c4a87fb3
Brandon <zupan.brandon@utexas.edu>
2020-01-08 17:17:10 -0600

    Add count neighbors

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
         pause();
 
 
-        //iterate_generations(&world, &num_rows, &num_cols);
+        iterate_generations(&world, &num_rows, &num_cols);
 
         show_world(&world);
     }
@@ -82,9 +82,46 @@ fn show_world(world: &Vec<String>) {
     }
 }
 
-//fn iterate_generations(world: &Vec<String>, num_rows: &i32, num_cols: &i32) {
-//
-//}
+fn count_neighbors(world: &Vec<String>, row: &i32, col: &i32, num_rows: &i32, num_cols: &i32) -> i32 {
+    //Counts the amount of alive neighbors that a cell has and returns it
+    //Make sure we are checking a valid spot
+    if (col < &0) || (col >= num_cols) || (row < &0) || (row >= num_rows) {
+        panic!("Error, tried to count neighbors of invalid spot, col:{} row:{}", col, row);
+    }
+    let mut neighbors = 0;
+    for col_index in -1..2 {
+        for row_index in -1..2 {
+            if row_index != 0 || col_index != 0 {
+                let line = match world.get((col + col_index) as usize) {
+                    Some(line) => line,
+                    None => continue,
+                };
+                //Turn it into a vector before working with it
+                let mut cells: Vec<char> = Vec::new();
+                for character in line.chars() {
+                    cells.push(character);
+                }
+                let cell = match cells.get((row + row_index) as usize) {
+                    Some(character) => character,
+                    None => continue,
+                };
+                //println!("Cell {}", cell);
+                if *cell == '*' {
+                    neighbors += 1;
+                }
+            }
+        }
+    }
+    return neighbors
+}
+
+fn iterate_generations(world: &Vec<String>, num_rows: &i32, num_cols: &i32) {
+    //Iterates to the next generation
+    let row = 0;
+    let col = 0;
+    let neighbors = count_neighbors(&world, &row, &col, &num_rows, &num_cols);
+    println!("Neighbors: {}", neighbors);
+}
 
 fn pause() {
     println!("Press enter to advance...");
